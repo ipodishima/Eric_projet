666202001d1b6ddd82e3caed7b654b1c637bff68
Élèves <eleves@Mac-mini-de-ISMIN-6.local>
2012-10-09 14:13:55 +0200

    initial import

---
+++ Eric_projet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Eric_projet
+//
+//  Created by Élèves on 09/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
